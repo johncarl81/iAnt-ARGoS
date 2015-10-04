b753fc1eed1c2160facbfedc6bcd49ed76eb4c63
lapatri <lpatrizi@msn.com>
2015-10-03 18:24:51 -0600

    Added generation max, min, and avg fitness to log.

--- source/main.cpp
+++ source/main.cpp
@@ -9,11 +9,14 @@
 #include "iAnt_controller.h"
 #include "iAnt_loop_functions.h"
 #include "iAnt_pheromone.h"
+#include <limits>
 
 /****************************************/
 /****************************************/
 
 int main(int argc, char** argv) {
+    int POPULATION = 10;
+    int NUM_GENERATIONS = 100;
 
     static argos::CSimulator& cSimulator = argos::CSimulator::GetInstance();
 
@@ -23,7 +26,8 @@ int main(int argc, char** argv) {
     iAnt_loop_functions &cLoopFunctions = (iAnt_loop_functions&)(cSimulator.GetLoopFunctions());
     BasicGA ga;
     ChromosomeFactory chromosomeFactory;
-    vector<Chromosome*> chromosomes = chromosomeFactory.buildPopulation(10);
+
+    vector<Chromosome*> chromosomes = chromosomeFactory.buildPopulation(POPULATION);
 
     //initial population always scores 0
     vector<BasicGA::FitnessChromosome> preFitness;
@@ -37,7 +41,8 @@ int main(int argc, char** argv) {
     chromosomes = ga.evolve(preFitness);
 
 
-    for(int i = 0; i < 100; i++) {
+
+    for(int i = 0; i < NUM_GENERATIONS; i++) {
         argos::LOG << "Generation #";
         argos::LOG << i;
         argos::LOG << " started.\n";
@@ -52,7 +57,9 @@ int main(int argc, char** argv) {
             dataOutput.close();
 
         vector<BasicGA::FitnessChromosome> fitness;
-
+        
+        int genMaxFitness = 0;
+        int genMinFitness = std::numeric_limits<int>::max();
         int fitsum = 0;
 
         for (int c = 0; c < chromosomes.size(); c++) {
@@ -64,15 +71,24 @@ int main(int argc, char** argv) {
             BasicGA::FitnessChromosome fitChromosome;
             fitChromosome.chromosome = chromosomes.at(c);
             fitChromosome.fitness = cLoopFunctions.getFitness();
+
             fitsum += fitChromosome.fitness;
             fitness.push_back(fitChromosome);
+
+            if (fitChromosome.fitness < genMinFitness) genMinFitness = fitChromosome.fitness;
+            if (fitChromosome.fitness > genMaxFitness) genMaxFitness = fitChromosome.fitness;
         }
+
+        float genAvg = (float)fitsum/(float)POPULATION;
 
         argos::LOG << "Generation #";
         argos::LOG << i;
-        argos::LOG << " total: ";
+        argos::LOG << ", total: ";
         argos::LOG << fitsum;
-        argos::LOG <<"done.\n";
+        argos::LOG << ", Min: "<< genMinFitness;
+        argos::LOG << ", Max: "<< genMaxFitness;
+        argos::LOG << ", AVG: "<< genAvg;
+        argos::LOG <<", done.\n";
 
         // This file is created in the directory where you run ARGoS
         // it is always created or appended to, never overwritten, i.e. ios::app
@@ -81,9 +97,12 @@ int main(int argc, char** argv) {
 
         dataOutput2 << "Generation #";
         dataOutput2 << i;
-        dataOutput2 << " total: ";
+        dataOutput2 << ", total: ";
         dataOutput2 << fitsum;
-        dataOutput2 <<" done.\n";
+        dataOutput2 << ", Min: "<< genMinFitness;
+        dataOutput2 << ", Max: "<< genMaxFitness;
+        dataOutput2 << ", AVG: "<< genAvg;
+        dataOutput2 <<", done.\n";
 
         chromosomes = ga.evolve(fitness);
     }
